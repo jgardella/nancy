ef8d7a84da511be48707a9f414d6c70aed695e4d
Jason Gardella <jason.a.gardella@gmail.com>
2017-01-14 20:20:52 -0500

    Setup cabal.

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,12 @@
 import Parser
 import Lexer
 import Typechecker
+import System.Environment
 import qualified Context.Truth as CT
 
 lexAndParse = calc . lexer
 
 typecheckStr str = (typecheckProgram . lexAndParse) str CT.empty
+
+main :: IO ()
+main = getArgs >>= print . typecheckStr . head
