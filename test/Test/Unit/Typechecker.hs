e203044a354908c16c8e00c4bee0203d21dc5e56
Jason Gardella <jason.a.gardella@gmail.com>
2017-08-28 18:31:29 -0400

    Add typechecker tests for applicaiton and variable lookup.

--- test/Test/Unit/Typechecker.hs
+++ test/Test/Unit/Typechecker.hs
@@ -6,7 +6,8 @@ import Test.Tasty.HUnit
 import AudiComp.Helper
 import AudiComp.Typechecker
 import AudiComp.Core.Language
-import AudiComp.Core.Errors.Typechecker as Err
+import AudiComp.Core.Errors
+import AudiComp.Core.Errors.Typechecker as TypecheckE
 
 typecheck = parseAndTypecheck "<test>"
 
@@ -14,7 +15,9 @@ typecheckerTests :: TestTree
 typecheckerTests = testGroup "Typechecker"
   [ intType
   , boolType
+  , idType
   , arrowType
+  , application
   ]
 
 intType =
@@ -39,27 +42,51 @@ boolType =
         (Right (BoolT, ConstantBoolW False))
   ]
 
+idType =
+  testGroup "Id"
+  [
+    testCase "Lookup of undefined variable" $
+      assertEqual ""
+        (typecheck "x")
+        (Left (TypecheckErr (TypecheckE.TruthVarUndefined "x")))
+  ]
+
 arrowType =
   testGroup "Arrow"
   [
-    testCase "Arrow type (constant body)" $
+    testCase "constant body" $
       assertEqual ""
         (typecheck "fun (x:int) -> true")
         (Right (ArrowT IntT BoolT, AbstractionW IntT (ConstantBoolW True)))
-  , testCase "Arrow type (parameter usage)" $
+  , testCase "parameter usage" $
       assertEqual ""
         (typecheck "fun (x:int) -> x")
         (Right (ArrowT IntT IntT, AbstractionW IntT (TruthHypothesisW IntT)))
   ]
 
-app =
+application =
   testGroup "Application"
   [
-    testCase "Function application" $
+    testCase "function application" $
       assertEqual ""
-        (typecheck "(fun x:int) -> true) 1")
+        (typecheck "(fun (x:int) -> true) 1")
         (Right (BoolT,
           ApplicationW
             (AbstractionW IntT (ConstantBoolW True))
             (ConstantIntW 1)))
+  , testCase "function application with lookup" $
+      assertEqual ""
+        (typecheck "(fun (x:int) -> x) 1")
+        (Right (IntT,
+          ApplicationW
+            (AbstractionW IntT (TruthHypothesisW IntT))
+            (ConstantIntW 1)))
+  , testCase "expected arrow" $
+    assertEqual ""
+      (typecheck "1 true")
+      (Left $ TypecheckErr (TypecheckE.ExpectedArrow (Number 1) IntT))
+  , testCase "invalid arg type" $
+    assertEqual ""
+      (typecheck "(fun (x:int) -> true) true")
+      (Left $ TypecheckErr (TypecheckE.InvalidArgType BoolT IntT))
   ]
