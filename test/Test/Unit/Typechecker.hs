f54e46b16a31e59ece7cfec87317fa43a22a3bde
Jason Gardella <jason.a.gardella@gmail.com>
2017-08-28 15:06:15 -0400

    Add bool and arrow type tests.

--- test/Test/Unit/Typechecker.hs
+++ test/Test/Unit/Typechecker.hs
@@ -11,10 +11,42 @@ typecheck = parseAndTypecheck "<test>"
 
 typecheckerTests :: TestTree
 typecheckerTests = testGroup "Typechecker"
-  [ intType ]
+  [ intType
+  , boolType
+  , arrowType
+  ]
 
 intType =
-  testCase "int type" $
-    assertEqual ""
-      (typecheck "1")
-      (Right (IntT, ConstantIntW 1))
+  testGroup "Int"
+  [
+    testCase "int type" $
+      assertEqual ""
+        (typecheck "1")
+        (Right (IntT, ConstantIntW 1))
+  ]
+
+boolType =
+  testGroup "Bool"
+  [
+    testCase "bool type (true)" $
+      assertEqual ""
+        (typecheck "true")
+        (Right (BoolT, ConstantBoolW True))
+  , testCase "bool type (false)" $
+      assertEqual ""
+        (typecheck "false")
+        (Right (BoolT, ConstantBoolW False))
+  ]
+
+arrowType =
+  testGroup "Arrow"
+  [
+    testCase "Arrow type (constant body)" $
+      assertEqual ""
+        (typecheck "fun (x:int) -> true")
+        (Right (ArrowT IntT BoolT, AbstractionW IntT (ConstantBoolW True)))
+  , testCase "Arrow type (parameter usage)" $
+      assertEqual ""
+        (typecheck "fun (x:int) -> x")
+        (Right (ArrowT IntT IntT, AbstractionW IntT (TruthHypothesisW IntT)))
+  ]
