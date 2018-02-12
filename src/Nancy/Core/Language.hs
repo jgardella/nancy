3867d4df6d015263e7274b6f0cb14811e9de8429
Jason Gardella <jason.a.gardella@gmail.com>
2018-02-12 10:17:42 -0500

    Simplify language constructs.

--- src/Nancy/Core/Language.hs
+++ src/Nancy/Core/Language.hs
@@ -8,105 +8,92 @@ data Type
   = IntT
   | BoolT
   | ArrowT Type Type
-  | BoxT String (Env Trail) Witness Type
-  | TrailReplacementT Type
+  | BoxT Witness Type
   deriving (Eq, Show)
 
 instance Pretty Type where
   pPrint IntT = text "int"
   pPrint BoolT = text "bool"
   pPrint (ArrowT l r) = pPrint l <+> text "->" <+> pPrint r
-  pPrint (BoxT _ e p t) = text "[" <> pPrint e <+> text "." <+> pPrint p <> text "]" <+> pPrint t
-  pPrint (TrailReplacementT t) = text "trl" <+> pPrint t
+  pPrint (BoxT p t) = text "[" <> pPrint p <> text "]" <+> pPrint t
 
 data Witness
-  = TruthHypothesisW String
-  | ConstantIntW Int
-  | ConstantBoolW Bool
-  | AbstractionW Type Witness
-  | ApplicationW Witness Witness
-  | ValidityHypothesisW String [TrailRename]
-  | BoxIntroductionW (Env Trail) Witness
-  | BoxEliminationW String Type Witness Witness
-  | TrailInspectionW String Witness Witness Witness Witness Witness Witness Witness Witness Witness
+  = VarWit String
+  | IntWit Int
+  | BoolWit Bool
+  | LamWit String Type Witness
+  | AppWit Witness Witness
+  | AVarWit String
+  | BangWit Witness
+  | LetWit String Type Witness Witness
+  | TiWit Witness Witness Witness Witness Witness Witness Witness Witness Witness
   deriving (Eq, Show)
 
 instance Pretty Witness where
-  pPrint (TruthHypothesisW s) = text s
-  pPrint (ConstantIntW i) = text (show i)
-  pPrint (ConstantBoolW b) = text (show b)
-  pPrint (AbstractionW t p) =
-      vcat [ text "fun" <+> pPrint t <+> text "->"
-           , nest 2 (pPrint p)]
-  pPrint (ApplicationW p1 p2) = pPrint p1 <> text " . " <> pPrint p2
-  pPrint (ValidityHypothesisW u trailRenames) = text "<" <> text u <> text ";" <+> pPrint trailRenames <> text ">"
-  pPrint (BoxIntroductionW e p) = pPrint e <+> text "." <+> pPrint p
-  pPrint (BoxEliminationW u t p1 p2) =
-    vcat [ text "LET("
-         , nest 2 (text u <> text ":" <> pPrint t <+> text "." <> pPrint p1 <> text ",")
-         , nest 2 (pPrint p2 <> text ")")]
-  pPrint (TrailInspectionW s p1 p2 p3 p4 p5 p6 p7 p8 p9) =
-    vcat [ text "r ->" <+> pPrint p1
-         , text "s ->" <+> pPrint p2
-         , text "t ->" <+> pPrint p3
-         , text "ba ->" <+> pPrint p4
-         , text "bb ->" <+> pPrint p5
-         , text "ti ->" <+> pPrint p6
-         , text "abs ->" <+> pPrint p7
-         , text "app ->" <+> pPrint p8
-         , text "let ->" <+> pPrint p9]
+  pPrint (VarWit s) = text "varwit(" <> text s <> text ")"
+  pPrint (IntWit i) = text "intwit(" <> text (show i) <> text ")"
+  pPrint (BoolWit b) = text "boolwit(" <> text (show b) <> text ")"
+  pPrint (LamWit s t p) = text "lamwit(" <> text s <> text ":" <> pPrint t <> text "," <+> pPrint p <> text ")"
+  pPrint (AppWit p1 p2) = text "appwit(" <> pPrint p1 <> text ", " <> pPrint p2 <> text ")"
+  pPrint (AVarWit u) = text "avarwit(" <> text u <> text ")"
+  pPrint (BangWit w) = text "bangwit(" <> pPrint w <> text")"
+  pPrint (LetWit u t p1 p2) =
+    text "letwit(" <> text u <> text ":" <> pPrint t <> text ","
+      <+> pPrint p1 <> text "," <+> pPrint p2 <> text ")"
+  pPrint (TiWit p1 p2 p3 p4 p5 p6 p7 p8 p9) =
+    text "tiwit("
+      <> pPrint p1 <> text ","
+      <+> pPrint p2 <> text ","
+      <+> pPrint p3 <> text ","
+      <+> pPrint p4 <> text ","
+      <+> pPrint p5 <> text ","
+      <+> pPrint p6 <> text ","
+      <+> pPrint p7 <> text ","
+      <+> pPrint p8 <> text ","
+      <+> pPrint p9 <> text ")"
 
 data Trail
     = EmptyT
-    | Reflexivity Witness
-    | Symmetry Trail
-    | Transitivity Trail Trail
-    | Beta Type Witness Witness
-    | BetaBox String Type Witness Witness
-    | AbsCompat Type Trail
-    | AppCompat Trail Trail
-    | LetCompat String Type Trail Trail
-    | TrailInspectionT String Witness Witness Witness Witness Witness Witness Witness Witness Witness
+    | RTrail Witness
+    | TTrail Trail Trail
+    | BaTrail String Type Witness Witness
+    | BbTrail String Type Witness Witness
+    | AbsTrail Type Trail
+    | AppTrail Trail Trail
+    | LetTrail String Type Trail Trail
+    | TiTrail Witness Witness Witness Witness Witness Witness Witness Witness Witness
     deriving (Eq, Show)
 
 instance Pretty Trail where
   pPrint EmptyT = text "emptyT"
-  pPrint (Reflexivity w) = text "r(" <> pPrint w <> text ")"
-  pPrint (Symmetry t) =
-    vcat [ text "s("
-         , nest 2 (pPrint t)
-         , text ")"]
-  pPrint (Transitivity e1 e2) =
-    vcat [ text "t("
-         , nest 2 (pPrint e1 <> text ",")
-         , nest 2 (pPrint e2)
-         , text ")"]
-  pPrint (Beta t w1 w2) =
+  pPrint (RTrail w) = text "r(" <> pPrint w <> text ")"
+  pPrint (TTrail e1 e2) = pPrint e1 <> text ";" <> pPrint e2
+  pPrint (BaTrail s t w1 w2) =
     vcat [ text "ba("
-         , nest 2 (text "a:" <> pPrint t <+> text "." <> pPrint w1 <> text ",")
+         , nest 2 (text s <> text ":" <> pPrint t <+> text "." <> pPrint w1 <> text ",")
          , nest 2 (pPrint w2)
          , text ")"]
-  pPrint (BetaBox u t w1 w2) =
+  pPrint (BbTrail u t w1 w2) =
     vcat [ text "bb("
          , nest 2 (text u <> text ":" <> pPrint t <+> text "." <> pPrint w1 <> text ",")
          , nest 2 (pPrint w2)
          , text ")"]
-  pPrint (AbsCompat t e) =
+  pPrint (AbsTrail t e) =
     vcat [ text "abs("
          , nest 2 (text "a:" <> pPrint t <+> text "." <> pPrint e)
          , text ")"]
-  pPrint (AppCompat e1 e2) =
+  pPrint (AppTrail e1 e2) =
     vcat [ text "app("
          , nest 2 (pPrint e1 <> text ",")
          , nest 2 (pPrint e2)
          , text ")"]
-  pPrint (LetCompat u t e1 e2) =
+  pPrint (LetTrail u t e1 e2) =
     vcat [ text "let("
          , nest 2 (text u <> text ":" <> pPrint t <+> text "." <> pPrint e1 <> text ",")
          , nest 2 (pPrint e2)
          , text ")"]
-  pPrint (TrailInspectionT s w1 w2 w3 w4 w5 w6 w7 w8 w9) =
-    vcat [ text "ti(" <> text s <> text ","
+  pPrint (TiTrail w1 w2 w3 w4 w5 w6 w7 w8 w9) =
+    vcat [ text "ti("
          , nest 2 (pPrint w1 <> text ",")
          , nest 2 (pPrint w2 <> text ",")
          , nest 2 (pPrint w3 <> text ",")
@@ -128,10 +115,10 @@ data Exp
   | Brack Exp
   | Abs String Type Exp
   | App Exp Exp
-  | AuditedVar [TrailRename] String
-  | AuditedUnit String Exp
+  | AuditedVar String
+  | AuditedUnit Exp
   | AuditedComp String Type Exp Exp
-  | TrailInspect String ReflexivityM SymmetryM TransitivityM BetaM BetaBoxM TrailInspectionM AbstractionM ApplicationM LetM
+  | TrailInspect ReflexivityM SymmetryM TransitivityM BetaM BetaBoxM TrailInspectionM AbstractionM ApplicationM LetM
   deriving (Eq, Show)
 
 newtype ReflexivityM = ReflexivityM Exp
@@ -152,15 +139,6 @@ data ApplicationM = ApplicationM String String Exp
   deriving (Eq, Show)
 data LetM = LetM String String Exp
   deriving (Eq, Show)
-
-data TrailRename = TrailRename {
-    old :: String
-  , new :: String
-  } deriving (Show, Eq)
-
-instance Pretty TrailRename where
-  pPrint TrailRename { old=old, new=new } =
-    text (old ++ "->" ++ new)
 
 data Value
   = IntV Int
